92d4ae5d8aea8d9676a63f949b30edb7ff1e8923
Kay <kaytran@nyu.edu>
2014-07-25 02:55:40 -0400

    Modified /js/modifiableScript.js to work with QR code given the identifier of the canvas element's parent (#original, #modifiable, #differences). Modified /js/modifiableScript.js #modifiable's QR code to receive hexvalue from mouse position and invert the color of that specific pixel module on #modifiable's QR code.

--- js/modifiableScript.js
+++ js/modifiableScript.js
@@ -1,58 +1,72 @@
 (function ($) {
 
-	// Recall: Version 1 has 21 modules. Each higher version number comprises 4 additional modules per side.	
-	var moduleCount;
+	var moduleCount,
+		black = "#000000", 
+		white = "#ffffff",
+		rgbToHex = function(r, g, b) {
+		    if (r > 255 || g > 255 || b > 255)
+		        throw "Invalid color component";
+		    return ((r << 16) | (g << 8) | b).toString(16);
+		},
+		canvasController = function(container){
+			var canvas = document.getElementById(container).children[0],
+				canvasContainer = $( "#" + container + " > canvas" ),
+				context = canvas.getContext('2d'),
+				canvasModuleLength = canvas.width / moduleCount,
+				containerModuleLength = canvasContainer.width() / moduleCount,
+				ratio = canvasModuleLength / containerModuleLength,
+				getMousePos = function (evt) {
+					var rect = canvas.getBoundingClientRect();
+					return {
+						x: evt.clientX - rect.left,
+						y: evt.clientY - rect.top
+					};
+				},
+
+				invertColor = function (mouse) {
+					// var context = canvas.getContext('2d');
+					// var canvasModuleLength = canvas.width / moduleCount;
+					// var containerModuleLength = canvasContainer.width() / moduleCount;
+					var xIndex = Math.floor( mouse.x / containerModuleLength ),
+						yIndex = Math.floor( mouse.y / containerModuleLength );
+
+					console.log( "moduleCount: " + moduleCount );
+					console.log( "canvas.width: " + canvas.width );
+					console.log( "container.width: " + canvasContainer.width() );
+					console.log( "canvasModuleLength: " + canvasModuleLength );
+					console.log( "containerModuleLength: " + containerModuleLength );
+					console.log( "Mouse: " + mouse.x + ", " + mouse.y );
+					console.log( "Index: " + xIndex + ", " + yIndex );
+
+					var mouseData = context.getImageData( mouse.x * ratio, mouse.y * ratio, 1, 1).data; 
+	    			var mouseHex = "#" + ("000000" + rgbToHex(mouseData[0], mouseData[1], mouseData[2])).slice(-6);
+	    			console.log( mouseHex );
+
+					context.fillStyle = ( mouseHex == white ) ? black : white;
+					context.fillRect( xIndex * canvasModuleLength, yIndex * canvasModuleLength, canvasModuleLength, canvasModuleLength );
+				},
+
+				reActivateCanvas = function(){
+					canvas.addEventListener('mousedown', function(evt) {
+						var mousePos = getMousePos(evt);
+						invertColor( mousePos );
+					}, false);
+				}
+			;
+			reActivateCanvas();
+		}
+	;
 
 	$(function () {
+		var reset = function(){
+			// Recall: Version 1 has 21 modules. Each higher version number comprises 4 additional modules per side
+			moduleCount = 21 + (parseInt($("#finalVer").text()) - 1) * 4;
+			canvasController('modifiable');
+			canvasController('differences');
+		};
 
-		var writeMessage = function (canvas, container, mouseX, mouseY) {
-				var context = canvas.getContext('2d');
-				var canvasModuleLength = canvas.width / moduleCount;
-				var containerModuleLength = container.width() / moduleCount;
-				var xIndex = Math.floor( mouseX  / containerModuleLength ),
-					yIndex = Math.floor( mouseY  / containerModuleLength );
-
-				console.log( "canvas.width: " + canvas.width );
-				// console.log( "container.width: " + container.css("width") );
-				console.log( "canvasModuleLength: " + canvasModuleLength );
-				console.log( "Mouse: " + mouseX + ", " + mouseY );
-				console.log( "Index: " + xIndex + ", " + yIndex );
-
-				// context.clearRect(0, 0, canvas.width, canvas.height);
-				// for (var y = 0; y < moduleCount; y++) {
-				// 	for (var x = 0; x < moduleCount; x++) {
-				// 		context.strokeRect( x*canvasModuleLength , y*canvasModuleLength, canvasModuleLength, canvasModuleLength);
-				// 	};
-				// };
-
-				context.fillStyle = "#FF0000";
-				context.fillRect( xIndex * canvasModuleLength, yIndex * canvasModuleLength, canvasModuleLength, canvasModuleLength );
-			},
-
-			getMousePos = function (canvas, evt) {
-				var rect = canvas.getBoundingClientRect();
-				return {
-					x: evt.clientX - rect.left + 0.484375,
-					y: evt.clientY - rect.top
-				};
-			},
-			reActivateCanvas = function(){
-				moduleCount = 21 + (parseInt($("#finalVer").text()) - 1)*4;
-
-				var canvas = document.getElementById('modifiable').children[0];
-				var container = $("#modifiable > canvas");
-				var context = canvas.getContext('2d');
-
-				canvas.addEventListener('click', function(evt) {
-					var mousePos = getMousePos(canvas, evt);
-					var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
-					writeMessage(canvas, container, mousePos.x, mousePos.y );
-				}, false);
-			}
-		;
-
-		reActivateCanvas();
-		$("#updateQR").on("click", reActivateCanvas);
+		reset();
+		$("#updateQR").on("click", reset);
 		
 	});
 
